1a87c0e801caf3ce801ac88436a939582650ad86
oheneadj <oheneadjei.dev@gmail.com>
2022-08-13 13:19:19 +0000

    added more sounds

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,47 @@
 const sounds = [
-    'i', 'ɪ','ʊ','u','ɛ','ə','ʌ','æ','ɑ','ɔ'
+  "i",
+  "ɪ",
+  "ʊ",
+  "u",
+  "ɛ",
+  "ə",
+  "ʌ",
+  "æ",
+  "ɑ",
+  "ɔ",
+  "ɛr",
+  "ɝ",
+  "aɪ",
+  "aʊ",
+  "ɑr",
+  "eɪ",
+  "ɚ",
+  "ɪr",
+  "oʊ",
+  "ɔɪ",
+  "ɔr",
+  "ʊr",
 ];
 
 sounds.forEach((sound) => {
-    const btn = document.createElement('button');
-    btn.classList.add('btn');
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
 
-    btn.innerText = sound;
+  btn.innerText = sound;
 
-    btn.addEventListener('click', () => {
-        stopSongs();
-        document.getElementById(sound).play()
-    })
+  btn.addEventListener("click", () => {
+    stopSongs();
+    document.getElementById(sound).play();
+  });
 
-    document.getElementById('buttons').appendChild(btn);
-})
+  document.getElementById("buttons").appendChild(btn);
+});
 
 const stopSongs = () => {
-    sounds.forEach(sound => {
-        const song = document.getElementById(sound);
+  sounds.forEach((sound) => {
+    const song = document.getElementById(sound);
 
-        song.pause();
-        song.currentTime = 0;
-    })
-}
+    song.pause();
+    song.currentTime = 0;
+  });
+};
